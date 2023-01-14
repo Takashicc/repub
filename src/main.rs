ebf04c8f21e671725d4976a76ef0d8028ff94317
Takashicc <105186894+Takashicc@users.noreply.github.com>
2023-01-14 17:26:04 +0900

    feat: Implement initial setup to run the cli

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+mod executor;
+mod params;
+use clap::{Parser, Subcommand};
+
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+struct Cli {
+    #[command(subcommand)]
+    command: Option<Commands>,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    /// Show rename commands for each epub file
+    Rename(params::rename::RenameParams),
+}
+
 fn main() {
-    println!("Hello, world!");
+    let cli = Cli::parse();
+    match &cli.command {
+        Some(Commands::Rename(v)) => {
+            executor::rename::execute(v);
+        }
+        None => eprintln!("No subcommand provided!\nCheck the subcommands with `repub -h`"),
+    }
 }
